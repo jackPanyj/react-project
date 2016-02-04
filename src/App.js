67cb759e170ccb2be66b9eb9d020299976892f37
jackPanyj <1016432619@qq.com>
2016-02-04 11:07:08 +0800

    material-ui test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React,{ Component } from "react";
 import Home from './Home';
+import NavBar from './shared/NavBar.js';
 import './style/main.scss';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 class App extends Component {
 
   render(){
     return (
         <div className="app-wrap">
+          <NavBar />
           <div className="app-header">my header</div>
           <Home />
           <div className= "app-footer"> my footer</div>
